ab59c8d9b8845e2b0bfc104c1f3c237e1370c4d5
Vivien Didelot <vivien.didelot@savoirfairelinux.com>
2017-09-01 16:33:00 -0400

    net: dsa: exclude CPU port when adding VLAN or MDB
    Now that a CPU port is programmable through its bridged master
    interface, do not automatically add it as a member of a VLAN or MDB
    entry. It is now the responsibility of the user to explicitly configure
    a switch CPU port through its master facing interface.
    
    Signed-off-by: Vivien Didelot <vivien.didelot@savoirfairelinux.com>

--- net/dsa/switch.c
+++ net/dsa/switch.c
@@ -121,7 +121,7 @@ static int dsa_switch_mdb_add(struct dsa_switch *ds,
 	if (ds->index == info->sw_index)
 		set_bit(info->port, group);
 	for (port = 0; port < ds->num_ports; port++)
-		if (dsa_is_cpu_port(ds, port) || dsa_is_dsa_port(ds, port))
+		if (dsa_is_dsa_port(ds, port))
 			set_bit(port, group);
 
 	if (switchdev_trans_ph_prepare(trans)) {
@@ -168,7 +168,7 @@ static int dsa_switch_vlan_add(struct dsa_switch *ds,
 	if (ds->index == info->sw_index)
 		set_bit(info->port, members);
 	for (port = 0; port < ds->num_ports; port++)
-		if (dsa_is_cpu_port(ds, port) || dsa_is_dsa_port(ds, port))
+		if (dsa_is_dsa_port(ds, port))
 			set_bit(port, members);
 
 	if (switchdev_trans_ph_prepare(trans)) {
