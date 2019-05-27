9102d098fd2f6f34edaf49b09f9d3a764d979a77
Vivien Didelot <vivien.didelot@gmail.com>
2019-05-27 15:32:52 -0400

    net: dsa: dump CPU port regs through master
    Merge the CPU port registers dump into the master interface registers
    dump through ethtool. To keep track of the CPU port ethtool data such
    as driver name, register dump length and version, embed ethtool_drvinfo
    and ethtool_regs structures into the dump.
    
    drvinfo->regdump_len will contain the full data length, which regs->len
    will contain only the master interface registers dump length.
    
    Signed-off-by: Vivien Didelot <vivien.didelot@gmail.com>

--- net/dsa/master.c
+++ net/dsa/master.c
@@ -8,6 +8,69 @@
 
 #include "dsa_priv.h"
 
+static int dsa_master_get_regs_len(struct net_device *dev)
+{
+	struct dsa_port *cpu_dp = dev->dsa_ptr;
+	const struct ethtool_ops *ops = cpu_dp->orig_ethtool_ops;
+	struct dsa_switch *ds = cpu_dp->ds;
+	int port = cpu_dp->index;
+	int ret = 0;
+	int len;
+
+	if (ops->get_regs_len) {
+		len = ops->get_regs_len(dev);
+		if (len < 0)
+			return len;
+		ret += len;
+	}
+
+	ret += sizeof(struct ethtool_drvinfo);
+	ret += sizeof(struct ethtool_regs);
+
+	if (ds->ops->get_regs_len) {
+		len = ds->ops->get_regs_len(ds, port);
+		if (len < 0)
+			return len;
+		ret += len;
+	}
+
+	return ret;
+}
+
+static void dsa_master_get_regs(struct net_device *dev, struct ethtool_regs *regs, void *data)
+{
+	struct dsa_port *cpu_dp = dev->dsa_ptr;
+	const struct ethtool_ops *ops = cpu_dp->orig_ethtool_ops;
+	struct dsa_switch *ds = cpu_dp->ds;
+	struct ethtool_drvinfo *einfo;
+	struct ethtool_regs *eregs;
+	int port = cpu_dp->index;
+	int len;
+
+	if (ops->get_regs_len && ops->get_regs) {
+		len = ops->get_regs_len(dev);
+		if (len < 0)
+			return;
+		regs->len = len;
+		ops->get_regs(dev, regs, data);
+		data += regs->len;
+	}
+
+	einfo = (struct ethtool_drvinfo *)data;
+	strlcpy(einfo->driver, "dsa", sizeof(einfo->driver));
+	data += sizeof(*einfo);
+	eregs = (struct ethtool_regs *)data;
+	data += sizeof(*eregs);
+
+	if (ds->ops->get_regs_len && ds->ops->get_regs) {
+		len = ds->ops->get_regs_len(ds, port);
+		if (len < 0)
+			return;
+		eregs->len = len;
+		ds->ops->get_regs(ds, port, eregs, data);
+	}
+}
+
 static void dsa_master_get_ethtool_stats(struct net_device *dev,
 					 struct ethtool_stats *stats,
 					 uint64_t *data)
@@ -147,6 +210,8 @@ static int dsa_master_ethtool_setup(struct net_device *dev)
 	if (cpu_dp->orig_ethtool_ops)
 		memcpy(ops, cpu_dp->orig_ethtool_ops, sizeof(*ops));
 
+	ops->get_regs_len = dsa_master_get_regs_len;
+	ops->get_regs = dsa_master_get_regs;
 	ops->get_sset_count = dsa_master_get_sset_count;
 	ops->get_ethtool_stats = dsa_master_get_ethtool_stats;
 	ops->get_strings = dsa_master_get_strings;
